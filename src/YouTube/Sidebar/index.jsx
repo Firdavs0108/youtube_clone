0e46e42824ea66283389a1c24faeff6243b00a11
Nazarov Firdavs <fredrickuzb55@gmail.com>
2023-06-06 23:35:53 +0900

    YouTube_clone

---
+++ src/YouTube/Sidebar/index.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Container } from '../Sidebar/style'
+
+export default class Sidebar extends Component {
+  render() {
+    return (
+      <Container>
+
+        <h1>Sidebar</h1>
+
+      </Container>
+    )
+  }
+}
---
+++ src/YouTube/style.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+
+export const Container = styled.div`
+display: flex;
+height: 100vh;
+background: #212121;
+color: #FFFFFF;
+`
